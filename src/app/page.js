7ae7339734c698972cb6158deb5e2a061aee0465
ashishthoppil <ashishthoppil23@gmail.com>
2025-08-21 10:13:09 +0530

    Whistle CSS changes

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,9 @@ export default function Home() {
 
   return (
     <div className='flex items-center justify-center h-screen'>
-        <button onClick={whistleHandler} className='lg:block  duration-300 bg-[#df6853]/15 text-[#DF6853] hover:text-white hover:bg-[#DF6853] font-bold text-2xl py-25 px-12 rounded-full hover:cursor-pointer'>
-            🔊 Whistle!
+        <button onClick={whistleHandler} className='flex gap-2 items-center justify-center duration-300 bg-[#df6853]/15 text-[#DF6853] hover:text-white hover:bg-[#DF6853] font-bold text-2xl py-24 px-12 rounded-full hover:cursor-pointer'>
+            <img src="/blow.png" width={30} height={30} />
+            Whistle!
         </button>
         <audio ref={audioRef} src="/whistle.mp3" />
     </div>
